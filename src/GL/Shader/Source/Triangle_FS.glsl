d394250156c1e2d6eba6659d0c33fc87dd3a4aef
Riari <3583774+Riari@users.noreply.github.com>
2020-10-03 11:54:11 +0100

    Implement model/view/projection matrices

--- src/GL/Shader/Source/Triangle_FS.glsl
+++ src/GL/Shader/Source/Triangle_FS.glsl
@@ -1,16 +1,13 @@
 #version 330 core
-in vec3 ourColor;
 in vec2 TexCoord;
 
 out vec4 FragColor;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-uniform sampler2D texture3;
-
 uniform float blend;
 
 void main()
 {
-    FragColor = mix(texture(texture2, TexCoord), texture(texture3, TexCoord), blend);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), blend);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -49,37 +49,76 @@ int main()
     triangleShaderProgram->DeleteShaders();
 
     float vertices[] = {
-        // positions         // colors           // texture coords
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
     VAO *vao = new VAO();
     VBO *vbo = new VBO();
-    EBO *ebo = new EBO();
 
     vao->Bind();
-
     vbo->Bind();
     vbo->SetData(sizeof(vertices), vertices, GL_STATIC_DRAW);
-    ebo->Bind();
-    ebo->SetData(sizeof(indices), indices, GL_STATIC_DRAW);
 
-    VBO::SetVertexAttribute(0, 3, 8 * sizeof(float), (void*)0); // position
-    VBO::SetVertexAttribute(1, 3, 8 * sizeof(float), (void*)(3 * sizeof(float))); // colour
-    VBO::SetVertexAttribute(2, 2, 8 * sizeof(float), (void*)(6 * sizeof(float))); // texture
+    VBO::SetVertexAttribute(0, 3, 5 * sizeof(float), (void*)0); // position
+    VBO::SetVertexAttribute(1, 2, 5 * sizeof(float), (void*)(3 * sizeof(float))); // texture
 
     auto *assetManager = new AssetManager();
     ImageRepository::SetFlipVerticallyOnLoad(true);
     std::shared_ptr<Image> containerImage = assetManager->GetImage("assets/textures/container.jpg");
     std::shared_ptr<Image> faceImage = assetManager->GetImage("assets/textures/febby.png");
-    std::shared_ptr<Image> dumbcatImage = assetManager->GetImage("assets/textures/dumbcat.png");
 
     auto containerTexture = new Texture(containerImage);
     containerTexture->Bind(GL_TEXTURE0);
@@ -95,55 +134,52 @@ int main()
     faceTexture->Define(true);
     assetManager->UnloadImage(faceImage->GetPath());
 
-    auto dumbcatTexture = new Texture(dumbcatImage);
-    dumbcatTexture->Bind(GL_TEXTURE2);
-    Texture::SetWrapMethod(GL_REPEAT);
-    Texture::SetFilterMethod(GL_LINEAR);
-    dumbcatTexture->Define(true);
-    assetManager->UnloadImage(dumbcatImage->GetPath());
-
     vbo->Unbind();
     VAO::Unbind();
-    ebo->Unbind();
 
     triangleShaderProgram->Use();
     triangleShaderProgram->SetUniformInt("texture1", 0);
     triangleShaderProgram->SetUniformInt("texture2", 1);
-    triangleShaderProgram->SetUniformInt("texture3", 2);
+
+    glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         containerTexture->Bind(GL_TEXTURE0);
         faceTexture->Bind(GL_TEXTURE1);
 
-        glm::mat4 trans = glm::mat4(1.0f);
+        // View matrix
+        glm::mat4 view = glm::mat4(1.0f);
+        // note that we're translating the scene in the reverse direction of where we want to move
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-        // First container
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        // Projection matrix
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-        triangleShaderProgram->SetUniformFloat("blend", 0.0);
-        triangleShaderProgram->SetUniformMatrix4fv("transform", glm::value_ptr(trans));
+        triangleShaderProgram->SetUniformFloat("blend", 0.5);
+        triangleShaderProgram->SetUniformMatrix4fv("view", glm::value_ptr(view));
+        triangleShaderProgram->SetUniformMatrix4fv("projection", glm::value_ptr(projection));
 
         vao->Bind();
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            // Model matrix
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = (i % 3 == 0) ? glfwGetTime() * 25.0f : i * 20.0f;
 
-        // Second container
-        trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.5f));
-        float scaleAmount = sin(glfwGetTime());
-        trans = glm::scale(trans, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 
-        triangleShaderProgram->SetUniformFloat("blend", 1.0);
-        triangleShaderProgram->SetUniformMatrix4fv("transform", &trans[0][0]);
+            triangleShaderProgram->SetUniformMatrix4fv("model", glm::value_ptr(model));
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -151,7 +187,6 @@ int main()
 
     delete vao;
     delete vbo;
-    delete ebo;
     delete assetManager;
     delete containerTexture;
     delete faceTexture;
